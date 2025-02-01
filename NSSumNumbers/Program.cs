5d71bdbfcb29f304bbd31a5f19d8d0d4c282004b
Zlodeyanin <evgeniy_zavarnitsin@mail.ru>
2025-02-01 16:04:43 +0300

    fix bags

--- NSSumNumbers/Program.cs
+++ NSSumNumbers/Program.cs
@@ -7,23 +7,26 @@ namespace NSSumNumbers
         static void Main(string[] args)
         {
             Random random = new Random();
-            int threeDivider = 3;
-            int fiveDivider = 5;
-            int number = random.Next(0,100);
+            int firstDivider = 3;
+            int secondDivider = 5;
+            int randomMinNumber = 0;
+            int randomMaxNumber = 100;
+            int number = random.Next(randomMinNumber,randomMaxNumber);
             int sum = 0;
 
             Console.WriteLine($"Рандомное число: { number} .");
-            Console.WriteLine($"Числа, кратные {threeDivider} и {fiveDivider} от числа {number} : ");
+            Console.WriteLine($"Числа, кратные {firstDivider} и {secondDivider} от числа {number} : ");
 
-            for (int i = 0; i < number; i++)
+            for (int i = 0; i <= number; i++)
             {
-                if(i % threeDivider == 0 || i % fiveDivider == 0)
+                if(i % firstDivider == 0 || i % secondDivider == 0)
                 {
                     Console.WriteLine(i);
                     sum += i;
                 }
             }
-            Console.WriteLine($"Сумма чисел кратных {threeDivider} и {fiveDivider} от числа {number} = {sum} .");
+
+            Console.WriteLine($"Сумма чисел кратных {firstDivider} и {secondDivider} от числа {number} = {sum} .");
         }
     }
 }
